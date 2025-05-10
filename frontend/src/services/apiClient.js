54d24dc93e4624b5370d370ebc3101863d1545c0
ocnaibill <biancocosta01@gmail.com>
2025-05-10 19:18:50 -0300

    fix: restauração de versão anterior do arquivo

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8080/api/";
+const apiUrl = "http://localhost:8080/api/";
 
 const apiClient = axios.create({
     baseURL: apiUrl,
